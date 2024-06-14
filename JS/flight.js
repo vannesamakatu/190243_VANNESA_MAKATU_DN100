a0ae1d0e157eb14d9548ecb99777758d00fef89b
V24code <190243@virtualwindow.co.za>
2024-06-14 02:44:27 +0200

    Final Red Planet Ventures
    Final website

--- JS/flight.js
+++ JS/flight.js
@@ -1,48 +1,13 @@
-// document.addEventListener("DOMContentLoaded", function() {
-  //  const flights = [
-     //   {
-     //      title: "The Classic Mars Experience",
-    //        price: "R 25,000.00",
-    //        description: "Embark on a Mars vacation: red deserts, ancient relics, and towering canyons await. Enjoy low-gravity hiking, rover trips to Olympus Mons, and stargazing under Phobos and Deimos. Unforgettable adventures beckon on the rust-colored landscapes of the Red Planet.",
-      //      sku: "SS001",
-      //      tags: ["Mars", "Luxury", "Sale"]
-        //},
-      //  {
-      //      title: "Neptune’s Cosmic Dive",
-       //     price: "R 45,000.00",
-       //     description: "Embark on a voyage to Neptune aboard the 'Neptune Voyager.' Witness surreal vistas of swirling clouds and rings. Experience weightlessness and explore frigid oceans. Engage in thrilling spacewalks, observe celestial phenomena, and savor gourmet space cuisine. An unforgettable adventure awaits, blending luxury with cosmic exploration.",
-       //     sku: "SS405",
-      //      tags: ["Neptune", "Commercial", "Gourmet Experience"]
-      //  },
-      //  {
-        //   title: "The Giant’s Journey",
-         //   price: "R 65,000.00",
-        //    description: "Embark on a Jupiter voyage aboard the 'Jupiter Odyssey.' Witness its majestic storms and swirling atmosphere. Dive into metallic hydrogen oceans. Explore its moons, each a world unto itself. Engage in gravity surfing, comet chasing, and space excursions. Experience cosmic wonder in luxury, blending exploration with adventure.",
-       //     sku: "SS606",
-       //     tags: ["Jupiter", "Luxury", "Adventure"]
-      //  },
-      //  {
-      //      title: "Ring Around a Saturn",
-     //       description: "Embark on a thrilling voyage to Saturn aboard the 'Saturn Explorer.' Marvel at its iconic rings and turbulent storms. Explore its diverse moons, from icy Enceladus to mysterious Titan, with activities including rover expeditions and cave exploration. Experience the wonders of the cosmos amidst luxurious accommodations, where the thrill of discovery intertwines seamlessly with the excitement of adventure.",
-       //     sku: "SS333",
-       //     tags: ["Saturn", "Luxury", "Adventure"]
-      //  },
-     //   {
-       //     title: "Uranus Uncharted",
-     //       price: "R 50,000.00",
-      //      description: "Embark on a cosmic adventure to Uranus with our 'Uranus Unveiled' tour. Explore its icy atmosphere and captivating moons. Experience thrilling zero-gravity excursions, witness mesmerizing auroras, and immerse yourself in celestial photography workshops. With luxury accommodations and expert guides, discover the wonders of Uranus in a journey of a lifetime.",
-      //      sku: "SS735",
-      //      tags: ["Uranus", "Workshop", "Adventure"]
-      //  },
-       // {
-       //     title: "Moonlight Meander",
-       //     price: "R 15,000.00",
-       //     description: "Experience the ultimate lunar getaway with our 'Moonlight Meander' package. Depart Earth's atmosphere aboard the 'Celestial Cruiser' for a short but unforgettable journey to the Moon. Enjoy lunar walks, breathtaking views of Earthrise, and stargazing under the moon's serene glow. Immerse yourself in luxury accommodations amidst the tranquility of space.",
-        //    sku: "SS451",
-       //     tags: ["Moon", "Commercial", "Romantic"]
-      //  }
-   // ];
+function filterTrips(criteria) {
+    
+    console.log("Filter by:", criteria);
+}
 
-   // const flightsContainer = document.getElementById("flights");
+function showTrips(count) {
+    
+    console.log("Show:", count, "trips");
+}
 
-    // I'm Still trying to figure out a function for the search and filter function to work out//
+function sortTrips(criteria) {
+    console.log("Sort by:", criteria);
+}
